1fdffc1177d7d319641867b27788b6a9d37f1a55
Suharto Banerjee <suharto.banerjee@mdc-berlin.de>
2024-04-20 01:24:52 +0200

    map rendered with a vec in a struct!
    Now I can query for the bin and get the sv_type! Time to implement game
    logic depending on the sv_type the player is on! Wohooo!!!

--- src/components.rs
+++ src/components.rs
@@ -1,3 +1,8 @@
+use std::any::Any;
+
+use bevy::input::keyboard::KeyboardInput;
+use bevy::math::f32;
+use bevy::scene::ron::de::Position;
 use bevy::window::PrimaryWindow;
 use bevy::prelude::*;
 use bevy_rapier2d::na::iter;
@@ -13,21 +18,14 @@ pub const PLAYER_SIZE: f32 = 70.0;
 
 #[derive(Component)]
 pub struct Player {
-    position: Landscape,
+    //position: Box<Landscape>,
     speed: f32,
     jump_impulse: f32,
     is_jumping: bool
 }
 
 pub fn spawn_player(mut commands: Commands,
-                    asset_server: Res<AssetServer>,
-                    landscape_query: Query<&Landscape>) {
-
-    let landscape = landscape_query.get_single().unwrap_or(&Landscape {
-        bin_id: 0,
-        sv_state: SvState::Ref
-    });
-
+                    asset_server: Res<AssetServer>) {
 
     commands.spawn(
         SpriteBundle{
@@ -37,13 +35,13 @@ pub fn spawn_player(mut commands: Commands,
         },
         )
         .insert(Player { 
-            position: landscape.to_owned(),
+            //position: Box::new(*landscape),
             speed: PLAYER_SPEED,
             jump_impulse: PLAYER_JUMP_IMPULSE,
             is_jumping: false
         })
         .insert(RigidBody::Dynamic)
-        .insert(Collider::cuboid(0.5, 0.5))
+        .insert(Collider::cuboid(0.1, 0.1))
         .insert(ColliderMassProperties::Mass(80.0))
         .insert(GravityScale(100.0))
         .insert(ActiveEvents::COLLISION_EVENTS)
@@ -71,20 +69,20 @@ pub fn spawn_camera(mut commands: Commands,
     );
 }
 
-pub fn player_movement(keyboard_input: Res<Input<KeyCode>>,
+pub fn player_movement(keyboard_input: Res<ButtonInput<KeyCode>>,
                        mut player_query: Query<(&mut Velocity, &mut Player), With<Player>>,
                        time: Res<Time>) {
 
     for (mut velocity, mut player) in player_query.iter_mut() {
         let mut direction = Vec2::ZERO;
 
-        if keyboard_input.pressed(KeyCode::Right) {
+        if keyboard_input.pressed(KeyCode::ArrowRight) {
             direction = Vec2::new(1.0, 0.0);
         }
-        if keyboard_input.pressed(KeyCode::Left) {
+        if keyboard_input.pressed(KeyCode::ArrowLeft) {
             direction = Vec2::new(-1.0, 0.0);
         }
-        if keyboard_input.pressed(KeyCode::Up) && !player.is_jumping {
+        if keyboard_input.pressed(KeyCode::ArrowUp) && !player.is_jumping {
             direction = Vec2::new(0.0, player.jump_impulse);
             player.is_jumping = true;
         }
@@ -92,6 +90,15 @@ pub fn player_movement(keyboard_input: Res<Input<KeyCode>>,
         velocity.linvel = direction * player.speed;
     }
 }
+
+pub fn update_bin_player(player_query: Query<(&Transform, &mut Player), With<Player>>) {
+    
+    for (position, player) in player_query.iter() {
+        let current_bin = (position.translation.x / 120.0).ceil();
+
+    }
+}
+
 
 
 pub fn set_jumping_false_if_touching_floor(mut contact_events: EventReader<CollisionEvent>,
@@ -108,7 +115,7 @@ pub fn set_jumping_false_if_touching_floor(mut contact_events: EventReader<Colli
 
 }
 
-pub fn camera_movement(keyboard_input: Res<Input<KeyCode>>,
+pub fn camera_movement(keyboard_input: Res<ButtonInput<KeyCode>>,
                        player_query: Query<&Velocity, With<Player>>,
                        mut camera_query: Query<&mut Transform, With<GameCamera>>,
                        time: Res<Time>) {
@@ -116,12 +123,11 @@ pub fn camera_movement(keyboard_input: Res<Input<KeyCode>>,
     if let Ok(mut transform) = camera_query.get_single_mut() {
         let mut direction = Vec3::ZERO;
         let player_x = player_query.get_single().unwrap().linvel.x;
-        println!("{:?}", &player_x);
 
-        if keyboard_input.pressed(KeyCode::Right) {
+        if keyboard_input.pressed(KeyCode::ArrowRight) {
             direction = Vec3::new(1.0, 0.0, 0.0)
         }
-        if keyboard_input.pressed(KeyCode::Left) {
+        if keyboard_input.pressed(KeyCode::ArrowLeft) {
             direction = Vec3::new(-1.0, 0.0, 0.0)
         }
 
@@ -129,7 +135,7 @@ pub fn camera_movement(keyboard_input: Res<Input<KeyCode>>,
             direction = direction.normalize();
         }
 
-        transform.translation += Vec3::new(player_x, 0.0, 0.0) * time.delta_seconds() * 0.6;
+        transform.translation += Vec3::new(player_x, 0.0, 0.0) * time.delta_seconds() * 0.5;
     }
 }
 
@@ -201,19 +207,31 @@ pub fn confine_camera_movement(mut camera_query: Query<&mut Transform, With<Game
 }
 
 #[derive(Debug, Clone, Copy)]
-enum SvState {
+pub enum SvState {
     Ref,
     Amp,
     Del,
-    Inv 
+    Inv
 }
 
 
-#[derive(Component, Clone, Copy)]
-pub struct Landscape {
+#[derive(Copy, Clone, Debug)]
+pub struct Land {
     bin_id: u32,
-    sv_state: SvState
+    sv_state: SvState,
 }
+
+#[derive(Resource, Clone, Debug)]
+pub struct Landscape {
+    map: Vec<Land>
+}
+
+impl Landscape {
+    fn new() -> Self {
+        return Self { map: Vec::new() };
+    }
+}
+
 
 pub fn spawn_landscape(mut commands: Commands,
                        asset_server: Res<AssetServer>) {
@@ -221,33 +239,53 @@ pub fn spawn_landscape(mut commands: Commands,
 
     let offset = 120.0;
     let rounds: u16 = 10;
-    let mut sv_type;
     let mut sprite_dir: &str;
+    let mut bins: u32;
+    let mut sv_type: SvState;
+    let mut landscape: Landscape = Landscape::new();
+
+
     for round in 0..rounds{
 
         if round < 5 {
             sv_type = SvState::Ref;
             sprite_dir = "sprites/roadTexture_13.png";
         } else {
-            sv_type = SvState::Inv;
+            sv_type = SvState::Del;
             sprite_dir = "sprites/roadTexture_85.png";
         }
 
-        // spawning the landscape
-        commands.spawn(
-                SpriteBundle{
-                    transform: Transform::from_xyz(BASE_ROAD.0 + offset * f32::from(round), BASE_ROAD.1, BASE_ROAD.2 - 1.0),
-                    texture: asset_server.load(sprite_dir),
-                    ..Default::default()
-                }
-            )
-            .insert(Landscape {
-                    bin_id: u32::from(round),
-                    sv_state: sv_type
-                },
-                )
-            .insert(RigidBody::Fixed)
-            .insert(Collider::cuboid(70.0, 70.0))
-            .insert(ActiveEvents::COLLISION_EVENTS);
+
+        let sprite_bundle = SpriteBundle{
+            transform: Transform::from_xyz(BASE_ROAD.0 + offset * f32::from(round), BASE_ROAD.1, BASE_ROAD.2 - 1.0),
+            texture: asset_server.load(sprite_dir),
+            ..Default::default()
+        };
+
+        landscape.map.push(Land { bin_id: round as u32, sv_state: sv_type });
+
+        commands.insert_resource(landscape.clone());
+
+        commands.spawn(sprite_bundle)
+        .insert(RigidBody::Fixed)
+        .insert(Collider::cuboid(70.0, 70.0))
+        .insert(ActiveEvents::COLLISION_EVENTS);
+        
     }
 }
+
+pub fn check_sv(player_query: Query<(&Transform, &Player)>,
+                landscape_query: Res<Landscape>) {
+
+
+    let player_position = player_query.get_single().expect("player_position not found").0.translation;
+    let current_bin = (player_position.x / 100.0).floor() as usize;
+    let sv_state = landscape_query.map[current_bin];
+
+    //println!("player: {}", player_position.x);
+    // println!("player: {:?}", player_query.get_single().unwrap().1.position);
+    println!("land: {:?}", sv_state);
+    // println!("land_type: {:?}", landscape_instance.1.sv_state);
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 use components::camera_movement;
+use components::check_sv;
 use components::confine_camera_movement;
 use components::confine_player_movement;
 use components::player_movement;
@@ -17,10 +18,11 @@ fn main() {
 
     App::new()
         .add_plugins((DefaultPlugins, RapierPhysicsPlugin::<NoUserData>::default()))
-        .add_systems(Startup, (spawn_camera, spawn_landscape, spawn_player))
+        .add_systems(Startup, (spawn_camera, spawn_landscape, spawn_player.after(spawn_landscape)))
         .add_systems(Update, (player_movement,
                               set_jumping_false_if_touching_floor,
                               camera_movement.after(player_movement),
+                              check_sv,
                               //confine_player_movement.after(player_movement),
                               confine_camera_movement))
         .run();
